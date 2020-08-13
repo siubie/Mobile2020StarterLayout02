373eb4fcdc906168afe85ec6c81983ac90b4d271
Putra Prima Arhandi <putraprima@gmail.com>
2020-08-13 07:40:14 +0700

    tambah activity pushup count

---
+++ app/src/main/java/id/putraprima/mobile2020starterlayout02/PushUpActivity.java
@@ -0,0 +1,14 @@
+package id.putraprima.mobile2020starterlayout02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PushUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_push_up);
+    }
+}
